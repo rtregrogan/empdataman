a4abeaff9c85c9480e80db6f74a21612ac458cf8
Rupert Tianzon <rtregrogan@gmail.com>
2021-03-10 12:02:17 -0800

    added setup page

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const ejsMate = require('ejs-mate');
 
 const port = process.env.PORT || 8000;
+
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
 app.listen(port, () => {
     console.log(`port is at ${port}`);
 });
 
-app.get('', (req, res) => {
-    res.send('Welcome to Employee Data Management: This app is under development')
-})
+app.get('/', (req, res) => {
+    res.render('setup')
+});
